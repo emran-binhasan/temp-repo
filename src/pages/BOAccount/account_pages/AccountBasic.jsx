6948d8e4e09acb73971234d623c757c23ef815b4
mk-saadi <mksaadi820@gmail.com>
2025-01-24 09:30:03 +0600

    minor change

--- src/pages/BOAccount/account_pages/AccountBasic.jsx
+++ src/pages/BOAccount/account_pages/AccountBasic.jsx
@@ -49,47 +49,22 @@ const AccountBasic = () => {
 	});
 	useScrollToTop();
 
-	// const handleChange = (e) => {
-	// 	const { name, value, type, files } = e.target;
-
-	// 	if (type === "file") {
-	// 		const file = files[0];
-	// 		if (file) {
-	// 			const reader = new FileReader();
-	// 			reader.onloadend = () => {
-	// 				setFormData((prevState) => ({
-	// 					...prevState,
-	// 					[name]: reader.result,
-	// 				}));
-	// 			};
-	// 			reader.readAsDataURL(file);
-	// 		}
-	// 	} else {
-	// 		setFormData((prevState) => ({
-	// 			...prevState,
-	// 			[name]: value,
-	// 		}));
-	// 	}
-	// };
 	const handleChange = (e) => {
 		const { name, value, type, files } = e.target;
-
 		if (type === "file") {
 			const file = files[0];
 			if (file) {
-				// Check if the file is over the size limit
 				if (file.size > 200 * 1024) {
 					new Compressor(file, {
-						quality: 0.8, // You can adjust quality here
-						maxWidth: 1000, // You can adjust the max width
-						maxHeight: 1000, // You can adjust the max height
+						quality: 0.8,
+						maxWidth: 1000,
+						maxHeight: 1000,
 						success(result) {
-							// Convert the compressed file to base64
 							const reader = new FileReader();
 							reader.onloadend = () => {
 								setFormData((prevState) => ({
 									...prevState,
-									[name]: reader.result, // Set the compressed image data
+									[name]: reader.result,
 								}));
 							};
 							reader.readAsDataURL(result);
@@ -99,7 +74,6 @@ const AccountBasic = () => {
 						},
 					});
 				} else {
-					// If the file is small enough, save it directly
 					const reader = new FileReader();
 					reader.onloadend = () => {
 						setFormData((prevState) => ({
@@ -110,6 +84,11 @@ const AccountBasic = () => {
 					reader.readAsDataURL(file);
 				}
 			}
+		} else {
+			setFormData((prevState) => ({
+				...prevState,
+				[name]: value,
+			}));
 		}
 	};
 
